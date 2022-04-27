f31b6648a11fd8a33f7404517a4b69b597ecfefb
Mayhew Seavey <commander.boffo@gmail.com>
2022-04-26 20:27:41 -0700

    MA-241: Make work for MediaMogul new DB

--- index.js
+++ index.js
@@ -21,11 +21,13 @@ const pool = new pg.Pool(options);
 
 exports.selectSendResponse = function(response, sql, values) {
 
-    pool.connect(function(err, client, done) {
-      if (err) {
-        console.error(err.message, err.stack);
-        response.send("Connection error " + err.message);
-      } else {
+  pool.connect(function(err, client, done) {
+    if (err) {
+      console.error(err.message, err.stack);
+      response.send("Connection error " + err.message);
+    } else {
+
+      client.query('SET SCHEMA \'mediamogul\'', _ => {
 
         client.query(sql, values, function (err, res) {
           done();
@@ -37,19 +39,23 @@ exports.selectSendResponse = function(response, sql, values) {
             return response.json(res.rows);
           }
         });
-      }
-    });
+      });
 
-  };
+    }
+  });
+
+};
 
 exports.updateSendResponse = function(response, sql, values) {
 
-    pool.connect(function(err, client, done) {
+  pool.connect(function(err, client, done) {
 
-      if (err) {
-        console.error(err.message, err.stack);
-        response.send("Connection error " + err.message);
-      } else {
+    if (err) {
+      console.error(err.message, err.stack);
+      response.send("Connection error " + err.message);
+    } else {
+
+      client.query('SET SCHEMA \'mediamogul\'', _ => {
 
         client.query(sql, values, function (err) {
           done();
@@ -61,18 +67,23 @@ exports.updateSendResponse = function(response, sql, values) {
             return response.json({msg: "Success"});
           }
         });
-      }
 
-    });
+      });
+    }
 
-  };
+  });
+
+};
 
 exports.updateNoResponse = function(sql, values) {
-    return new Promise(function(resolve, reject) {
-      pool.connect(function(err, client, release) {
-        if (err) {
-          reject(err);
-        } else {
+  return new Promise(function(resolve, reject) {
+    pool.connect(function(err, client, release) {
+      if (err) {
+        reject(err);
+      } else {
+
+        client.query('SET SCHEMA \'mediamogul\'', _ => {
+
           client.query(sql, values, function(err) {
             release();
             if (err) {
@@ -81,17 +92,22 @@ exports.updateNoResponse = function(sql, values) {
               resolve("Success!");
             }
           });
-        }
-      });
+
+        });
+      }
     });
-  };
+  });
+};
 
 exports.selectNoResponse = function(sql, values) {
-    return new Promise(function (resolve, reject) {
-      pool.connect(function (err, client, release) {
-        if (err) {
-          reject(err);
-        } else {
+  return new Promise(function (resolve, reject) {
+    pool.connect(function (err, client, release) {
+      if (err) {
+        reject(err);
+      } else {
+
+        client.query('SET SCHEMA \'mediamogul\'', _ => {
+
           client.query(sql, values, function (err, results) {
             release();
             if (err) {
@@ -100,112 +116,114 @@ exports.selectNoResponse = function(sql, values) {
               resolve(results.rows);
             }
           });
-        }
-      });
+
+        });
+      }
     });
-  };
+  });
+};
 
 exports.buildUpdateQueryConfig = function(changedFields, tableName, rowID) {
 
-    let sql = "UPDATE " + tableName + " SET ";
-    let values = [];
-    let i = 1;
-    for (let key in changedFields) {
-      if (changedFields.hasOwnProperty(key)) {
-        if (values.length !== 0) {
-          sql += ", ";
-        }
-
-        sql += (key + " = $" + i);
-
-        const value = changedFields[key];
-        values.push(value);
-
-        i++;
+  let sql = "UPDATE " + tableName + " SET ";
+  let values = [];
+  let i = 1;
+  for (let key in changedFields) {
+    if (changedFields.hasOwnProperty(key)) {
+      if (values.length !== 0) {
+        sql += ", ";
       }
+
+      sql += (key + " = $" + i);
+
+      const value = changedFields[key];
+      values.push(value);
+
+      i++;
     }
+  }
 
-    sql += (" WHERE id = $" + i);
+  sql += (" WHERE id = $" + i);
 
-    values.push(rowID);
+  values.push(rowID);
 
-    return {
-      text: sql,
-      values: values
-    };
+  return {
+    text: sql,
+    values: values
   };
+};
 
 exports.buildUpdateQueryConfigNoID = function(changedFields, tableName, identifyingColumns) {
 
-    let sql = "UPDATE " + tableName + " SET ";
-    let values = [];
-    let i = 1;
-    for (let key in changedFields) {
-      if (changedFields.hasOwnProperty(key)) {
-        if (values.length !== 0) {
-          sql += ", ";
-        }
-
-        sql += (key + " = $" + i);
-
-        const value = changedFields[key];
-        values.push(value);
-
-        i++;
+  let sql = "UPDATE " + tableName + " SET ";
+  let values = [];
+  let i = 1;
+  for (let key in changedFields) {
+    if (changedFields.hasOwnProperty(key)) {
+      if (values.length !== 0) {
+        sql += ", ";
       }
+
+      sql += (key + " = $" + i);
+
+      const value = changedFields[key];
+      values.push(value);
+
+      i++;
     }
+  }
 
-    const lengthBeforeWheres = values.length;
+  const lengthBeforeWheres = values.length;
 
-    sql += " WHERE ";
+  sql += " WHERE ";
 
-    for (key in identifyingColumns) {
-      if (identifyingColumns.hasOwnProperty(key)) {
-        if (values.length !== lengthBeforeWheres) {
-          sql += " AND ";
-        }
-
-        sql += (key + " = $" + i);
-
-        value = identifyingColumns[key];
-        values.push(value);
-
-        i++;
+  for (key in identifyingColumns) {
+    if (identifyingColumns.hasOwnProperty(key)) {
+      if (values.length !== lengthBeforeWheres) {
+        sql += " AND ";
       }
-    }
 
-    return {
-      text: sql,
-      values: values
-    };
+      sql += (key + " = $" + i);
+
+      value = identifyingColumns[key];
+      values.push(value);
+
+      i++;
+    }
+  }
+
+  return {
+    text: sql,
+    values: values
   };
+};
 
 exports.updateObjectWithChangedFieldsSendResponse = function(response, changedFields, tableName, rowID) {
-    console.log("Update " + tableName + " with " + JSON.stringify(changedFields));
+  console.log("Update " + tableName + " with " + JSON.stringify(changedFields));
 
-    const queryConfig = this.buildUpdateQueryConfig(changedFields, tableName, rowID);
+  const queryConfig = this.buildUpdateQueryConfig(changedFields, tableName, rowID);
 
-    console.log("SQL: " + queryConfig.text);
-    console.log("Values: " + queryConfig.values);
+  console.log("SQL: " + queryConfig.text);
+  console.log("Values: " + queryConfig.values);
 
-    return this.updateSendResponse(response, queryConfig.text, queryConfig.values);
-  };
+  return this.updateSendResponse(response, queryConfig.text, queryConfig.values);
+};
 
 exports.updateObjectWithChangedFieldsNoResponse = function(changedFields, tableName, rowID) {
-    console.log("Update " + tableName + " with " + JSON.stringify(changedFields));
+  console.log("Update " + tableName + " with " + JSON.stringify(changedFields));
 
-    const queryConfig = this.buildUpdateQueryConfig(changedFields, tableName, rowID);
+  const queryConfig = this.buildUpdateQueryConfig(changedFields, tableName, rowID);
 
-    console.log("SQL: " + queryConfig.text);
-    console.log("Values: " + queryConfig.values);
+  console.log("SQL: " + queryConfig.text);
+  console.log("Values: " + queryConfig.values);
 
-    return this.updateNoResponse(queryConfig.text, queryConfig.values);
-  };
+  return this.updateNoResponse(queryConfig.text, queryConfig.values);
+};
 
 exports.createInlineVariableList = function(arrSize, starting) {
-    let varNumbers = [];
-    for (let i = starting; i < (starting + arrSize); i++) {
-      varNumbers.push('$' + i);
-    }
-    return varNumbers.join(', ');
-  };
+  let varNumbers = [];
+  for (let i = starting; i < (starting + arrSize); i++) {
+    varNumbers.push('$' + i);
+  }
+  return varNumbers.join(', ');
+};
